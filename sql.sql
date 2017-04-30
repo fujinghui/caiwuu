ebbcd2c66f98cd9bbf9ededce72265bc1ff93a9e
fujinghui <772725447@qq.com>
2017-04-30 14:50:46 +0800

    new sql.sql

---
+++ sql.sql
@@ -0,0 +1,8 @@
+﻿create database caiwuu;
+alter database caiwuu character set utf8
+
+create table user_info(
+	user_id int primary key auto_increment,
+	user_name varchar(32) not null,
+	user_password varchar(32) not null
+)default charset=utf8;
